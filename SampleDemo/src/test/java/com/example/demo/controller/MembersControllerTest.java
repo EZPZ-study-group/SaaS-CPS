c9430f9d04affc940ea29e25ab393f3557209ef8
유승민 <yu3096@uracle.co.kr>
2023-11-24 16:44:55 +0900

    spring mvc get test

--- SampleDemo/src/test/java/com/example/demo/controller/MembersControllerTest.java
+++ SampleDemo/src/test/java/com/example/demo/controller/MembersControllerTest.java
@@ -1,7 +1,26 @@
 package com.example.demo.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 class MembersControllerTest {
-
+    @Autowired
+    private MockMvc mvc;
+    @Test
+    @DisplayName("method: Get")
+    public void callGetTest() throws Exception {
+        this.mvc.perform(get("/members"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(print());
+    }
 }
